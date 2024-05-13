b6b0f0f6988ca7962701f6b21c5173a31940423a
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-05-13 14:27:58 +0900

    Delete SQL CODE KATA/16번 경기도에 위치한 식품창고 목록 출력하기.sql

--- SQL CODE KATA/16번 경기도에 위치한 식품창고 목록 출력하기.sql
+++
@@ -1,7 +0,0 @@
-SELECT warehouse_id,
-       warehouse_name,
-       address,
-       IF (freezer_yn IS NULL, 'N', freezer_yn) AS FREEZER_YN
-FROM food_warehouse
-WHERE address LIKE '%경기%'
-ORDER BY warehouse_id 
