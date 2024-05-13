5b1912a48c3aa4a62fd90514e33404dfcc69066a
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-05-13 14:27:27 +0900

    Delete SQL CODE KATA/15번 null 처리하기.sql

--- SQL CODE KATA/15번 null 처리하기.sql
+++
@@ -1,7 +0,0 @@
-SELECT animal_type,
-       IF(name IS NULL, 'No name', name),
-       sex_upon_intake
-FROM animal_ins
-ORDER BY animal_id
-
-# https://inpa.tistory.com/entry/MYSQL-%F0%9F%93%9A-%EC%A0%9C%EC%96%B4%EB%AC%B8#if%EB%AC%B8 IF문 참고 블로그
