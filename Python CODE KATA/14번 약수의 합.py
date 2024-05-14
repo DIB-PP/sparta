af30e51010265bea5196c6f9d87f021d9dce4e3f
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-05-14 16:09:08 +0900

    Delete Python CODE KATA/14번 약수의 합.py

--- Python CODE KATA/14번 약수의 합.py
+++
@@ -1,8 +0,0 @@
-def solution(n):
-    answer = 0
-    for i in range(1, n+1):
-        if n % i == 0:
-            answer += i
-    return answer
-
-# https://sun-kakao.tistory.com/96 약수 구하기 참고 블로그 
