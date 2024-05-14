0ed205c6d4c5a816e750b027e927443a032492eb
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-05-14 16:09:01 +0900

    Delete Python CODE KATA/13번 자릿수 더하기.py

--- Python CODE KATA/13번 자릿수 더하기.py
+++
@@ -1,8 +0,0 @@
-def solution(n):
-    answer = 0
-    for i in map(int, str(n)):
-        answer += i
-    return answer
-
-# https://dotiromoook.tistory.com/28 map 함수 설명 및 사용법 참고 블로그 
-# map 함수 한 번 더 이해 필요 
