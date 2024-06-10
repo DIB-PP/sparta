d4f9de6785e79dd111c3d241069350f83b4d9bc5
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-06-10 14:59:02 +0900

    Create 조건별로 분류하여 주문상태 출력하기.sql

---
+++ SQL CODE KATA/240610/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,9 @@
+SELECT order_id
+     , product_id
+     , DATE_FORMAT(out_date, '%Y-%m-%d') AS out_date
+     , CASE WHEN DATE_FORMAT(out_date, '%Y-%m-%d') <= '2022-05-01' THEN '출고완료'
+            WHEN DATE_FORMAT(out_date, '%Y-%m-%d') > '2022-05-01' THEN '출고대기'
+            ELSE '출고미정'
+       END AS '출고여부'
+FROM food_order
+ORDER BY order_id
