c3368f8496b837a4e046fcf3f0b300021656c7b9
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-06-25 11:50:12 +0900

    Create 식품분류별 가장 비싼 식품의 정보 조회하기.sql

---
+++ SQL CODE KATA/240625/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT category
+     , price AS max_price
+     , product_name
+FROM food_product
+WHERE (category, price) IN (SELECT category
+                                 , MAX(price) AS price
+                            FROM food_product
+                            GROUP BY category)
+    AND category IN ('과자', '국', '김치', '식용유')
+ORDER BY max_price DESC
