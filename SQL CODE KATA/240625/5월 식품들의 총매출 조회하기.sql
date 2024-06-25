64ff738aaecdaddd0867065e614586722ec9015d
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-06-25 11:52:23 +0900

    Update 5월 식품들의 총매출 조회하기.sql

--- SQL CODE KATA/240625/5월 식품들의 총매출 조회하기.sql
+++ SQL CODE KATA/240625/5월 식품들의 총매출 조회하기.sql
@@ -1,3 +1,4 @@
+1차 풀이
 SELECT product_id
      , product_name
      , SUM(total_sales) AS total_sales
@@ -6,5 +7,16 @@ FROM (SELECT product.product_id
             , product.price * food.amount AS total_sales
       FROM food_product AS product JOIN food_order AS food ON product.product_id = food.product_id
       WHERE DATE_FORMAT(food.produce_date, '%Y-%m') = '2022-05') total
+GROUP BY product_id
+ORDER BY total_sales DESC, product_id
+
+-----
+
+2차 풀이
+SELECT product.product_id
+     , product.product_name
+     , SUM(product.price * food.amount) AS total_sales
+FROM food_product AS product JOIN food_order AS food ON product.product_id = food.product_id
+WHERE DATE_FORMAT(food.produce_date, '%Y-%m') = '2022-05'
 GROUP BY product_id
 ORDER BY total_sales DESC, product_id
