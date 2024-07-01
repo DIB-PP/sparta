2a6a8c67392f7709fec83c5582d84924dff74535
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-07-01 16:17:56 +0900

    Create 과제1.sql

---
+++ SYstudy/SQL1/과제1.sql
@@ -0,0 +1,17 @@
+-- 모든 데이터 컬럼 추출
+SELECT *
+FROM users ;
+
+-- 데이터 중 date 컬럼만 추출하되, 이름을 yearmonthday 로 변경하여 추출
+SELECT date AS yearmonthday
+FROM users ;
+
+-- 게임계정id 갯수 추출(중복값을 허용, 중복값 제외를 동시에 추출해주세요)
+SELECT COUNT(game_account_id)
+	   	, COUNT(DISTINCT(game_account_id))
+FROM users ;
+
+-- 캐릭터id 갯수 추출(중복값을 허용, 중복값 제외를 동시에 추출해주세요)
+SELECT COUNT(game_actor_id)
+		, COUNT(DISTINCT(game_actor_id))
+FROM users ;
